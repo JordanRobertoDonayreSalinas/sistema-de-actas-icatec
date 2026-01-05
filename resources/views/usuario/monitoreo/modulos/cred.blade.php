c2d9291bb13509f25fccbea081227cda31cf6169
jairom123 <40218628+jairom123@users.noreply.github.com>
2026-01-05 08:28:27 -0500

    cambios

--- resources/views/usuario/monitoreo/modulos/cred.blade.php
+++ resources/views/usuario/monitoreo/modulos/cred.blade.php
@@ -276,27 +276,6 @@
                             <input type="file" name="foto_evidencia_1" id="input_foto_1" @change="previewImage($event, 'img1')" class="absolute inset-0 opacity-0 cursor-pointer">
                         </div>
                     </div>
-
-                    <div class="space-y-4">
-                        <label class="text-[10px] font-black uppercase text-slate-400 ml-2 italic">Evidencia 02</label>
-                        <div class="relative h-48 rounded-3xl border-2 border-dashed border-slate-200 overflow-hidden bg-slate-50 flex items-center justify-center">
-                            <template x-if="images.img2">
-                                <div class="relative w-full h-full group">
-                                    <img :src="images.img2" class="h-full w-full object-cover">
-                                    <button type="button" @click="removeImage('img2', 'input_foto_2', 'foto_2_actual')" class="btn-remove-image">
-                                        <i data-lucide="x" class="w-4 h-4"></i>
-                                    </button>
-                                </div>
-                            </template>
-                            <template x-if="!images.img2">
-                                <div class="flex flex-col items-center gap-2 text-slate-300">
-                                    <i data-lucide="image-plus" class="w-10 h-10"></i>
-                                    <span class="text-[10px] font-black uppercase">Subir Foto</span>
-                                </div>
-                            </template>
-                            <input type="file" name="foto_evidencia_2" id="input_foto_2" @change="previewImage($event, 'img2')" class="absolute inset-0 opacity-0 cursor-pointer">
-                        </div>
-                    </div>
                 </div>
 
                 <div class="pt-10">
